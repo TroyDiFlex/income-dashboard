bd6671cbfbbead0233aece15cd8820856a7eb1e8
TroyDiFlex <159740011+TroyDiFlex@users.noreply.github.com>
2026-09-03 03:12:17 +0400

    Connect deployed Google Apps Script income API

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Public configuration only. Never put passwords or income data here.
 export const CONFIG = Object.freeze({
   appName: 'Поток',
-  apiUrl: '',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbztRjupojGfOWD1cEbUWwyp1RlyW7qSbVhrIFIxnossXatcfLf1eJyR6aY8mbk5Kwck/exec',
   repository: 'TroyDiFlex/income-dashboard',
   sessionHours: 6,
 });
